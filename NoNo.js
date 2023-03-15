d618d1c6948b6e83bfdb6f9c8dfc5a32343e3caa
NoNo <103244260+NoNoTheGoat@users.noreply.github.com>
2023-03-15 08:39:36 -0700

    Create NoNo.js

---
+++ NoNo.js
@@ -0,0 +1,30 @@
+/**
+ * @name NoNo's Personal Wallpaper
+ * @author NoNo
+ * @version 1.0 Not To Be Updated
+ * @description This is NoNo's background for better discord as of Mar, 2023
+ * @source https://github.com/NoNoTheGoat/BD-NoNo-s-Background
+ */
+
+@import url('https://raw.githack.com/NoNoTheGoat/BD-NoNo-s-Background/main/transparency2.css');
+@import url('https://raw.githack.com/NoNoTheGoat/BD-NoNo-s-Background/main/core.css');
+
+:root{
+    --background: url('https://media.discordapp.net/attachments/1004286758206316554/1064969137912299650/untitled.png?width=1213&height=682');
+    --activetext-colour: white;
+
+    --primary1-colour: red;
+    --primary2-colour: rgba(255,0,0,0.6);
+    --primary3-colour: rgba(100,0,0,0.6);
+
+    --secondary1-colour: rgba(50,50,50,0.3);
+    --secondary2-colour: rgba(100,100,100,0.5);
+    --secondary3-colour: rgba(0,0,0,0.5);
+
+    --transparency-1: linear-gradient(to right, rgba(0,0,0,0.5), rgba(0,0,0,0.5));
+    --transparency-2: linear-gradient(to right, rgba(0,0,0,0.5), rgba(0,0,0,0.5));
+    --transparency-3: linear-gradient(to right, rgba(0,0,0,0.5), rgba(0,0,0,0.5));
+    --transparency-4: linear-gradient(to right, rgba(0,0,0,0.5), rgba(0,0,0,0.5));
+    --transparency-5: linear-gradient(to right, rgba(0,0,0,0.5), rgba(0,0,0,0.5));
+}
+  
